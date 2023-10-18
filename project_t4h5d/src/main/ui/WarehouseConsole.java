7b0cde5596bbe62ace5d2d1d9a9c22b8dc8db1ca
Daniel Huang <jiakang2022@126.com>
2023-10-18 00:18:51 -0700

    Delete project_t4h5d directory

--- project_t4h5d/src/main/ui/WarehouseConsole.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-// Main WarehouseConsole
-public class WarehouseConsole {
-    public static void main(String[] args) {
-        new LoginSystem();
-        new WarehouseManagementSystem();
-    }
-}
--- project_t4h5d/src/main/ui/WarehouseGui.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-// Main WarehouseGui
-public class WarehouseGui  {
-
-    public static void main(String[] args) {
-        new LoginSystemGui();
-    }
-}
